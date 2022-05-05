485b5216f69229c63d372960da7b59b164689db2
dogitteamuserzero <104084598+dogitteamuserzero@users.noreply.github.com>
2022-05-05 15:39:52 +0300

    Make stupid error

--- src/main/resources/archetype-resources/src/main/java/App.java
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -1,6 +1,10 @@
 package ${groupId};
 
 public class App {
+    public static int reco (String MyParam){
+        reco = 55;
+    }
+    
 	public static void main (String... args) {
 		System.out.println("App is running");
 	}
